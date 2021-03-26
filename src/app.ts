ff1a3982d4d717163d7280c84b71a18eb38cae2f
Nauja <jeremymorosi@hotmail.fr>
2021-03-26 15:02:50 +0100

    prettier

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
-import * as net from "net"
+import * as net from "net";
 
 const Server = require("jsonrpc-node").TCP.Server;
 
-function ping(args: any, reply: any)
-{
+function ping(args: any, reply: any) {
     reply("pong");
 }
 
-export function create_server(ready?: () => void): net.Server
-{
+export function create_server(ready?: () => void): net.Server {
     // Create a JSON-RPC server
     var server = new Server();
 
@@ -18,12 +16,14 @@ export function create_server(ready?: () => void): net.Server
     // Wrap our server with the builtin net.Server
     // We could also start our server standalone
     server = net.createServer(server);
-    
+
     // Start the server on 0.0.0.0 host with a random port
-    server.listen({host: "0.0.0.0", port: 0}, function() {
+    server.listen({ host: "0.0.0.0", port: 0 }, function () {
         // Called when the server is ready
         const port = (server.address() as net.AddressInfo).port;
-        console.log(`Server listening for connection requests on socket 0.0.0.0:${port}`);
+        console.log(
+            `Server listening for connection requests on socket 0.0.0.0:${port}`
+        );
 
         // Notify our server is ready if a callback was given
         if (ready !== undefined) {
