1acc4675b4e8907a61271f12ce9953ddd9de81aa
Nauja <jeremymorosi@hotmail.fr>
2021-03-26 11:56:18 +0100

    wip test

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
-const net = require("net");
+import * as net from "net"
 const jsonrpc = require("jsonrpc-node");
 
-var server = new jsonrpc.TCP.Server();
-// Create a new TCP server.
-server = net.createServer(server);
-// The port on which the server is listening.
-const port = 8080;
+function ping(args: any, reply: any)
+{
+    reply("pong");
+}
 
-// The server listens to a socket for a client to make a connection request.
-// Think of a socket as an end point.
-server.listen(port, function() {
-    console.log(`Server listening for connection requests on socket localhost:${port}`);
-});
+export function create_server(ready?: () => void): net.Server
+{
+    var server = new jsonrpc.TCP.Server();
 
-export default server;
+    server.register("ping", ping);
+
+    // Create a new TCP server.
+    server = net.createServer(server);
+    
+    // The server listens to a socket for a client to make a connection request.
+    // Think of a socket as an end point.
+    server.listen({host: "0.0.0.0", port: 0}, function() {
+        const port = (server.address() as net.AddressInfo).port;
+        console.log(`Server listening for connection requests on socket 0.0.0.0:${port}`);
+        if (ready !== undefined) {
+            ready();
+        }
+    });
+
+    return server;
+}
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,7 +1,25 @@
-import app from "../src/app";
+import * as net from "net"
+import * as app from "../src/app";
 
-describe("GET /random-url", () => {
-    it("should return 404", (done) => {
-        done();
+const Client = require("jsonrpc-node").TCP.Client;
+
+// Describe a sequence of tests
+describe("test server RPCs", () => {
+    // This is a single test
+    it("call ping should send pong", (done) => {
+        // Start a new server for this test and wait to be ready
+        const server = app.create_server(() => {
+            // Connect a client to started server
+            const port = (server.address() as net.AddressInfo).port;
+            console.log(`server port is ${port}`);
+            const client = new Client(port, "127.0.0.1");
+            // Call the ping function with RPC
+            client.call("ping", [], (err: any, result: any) => {
+                // Check the received result is equals to "pong"
+                expect(result).toEqual("pong");
+                // Notify this test is done
+                done();
+            })
+        });
     });
 });
